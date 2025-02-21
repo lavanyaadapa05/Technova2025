b43674342ebd16fdb23f2a261bd078d61ce83562
lavanyaadapa05 <lavanyaadapa05@gmail.com>
2025-02-21 14:47:02 +0530

    navigate

--- front/src/shooting-stars-and-stars-background-demo.jsx
+++ front/src/shooting-stars-and-stars-background-demo.jsx
@@ -32,7 +32,7 @@ export default function ShootingStarsAndStarsBackgroundDemo() {
         {/* Register Button */}
         <button 
           className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-lg font-semibold leading-6 text-white inline-block"
-          onClick={() => navigate("/register")}
+          onClick={() => navigate("/registration")}
         >
           <span className="absolute inset-0 overflow-hidden rounded-full">
             <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
