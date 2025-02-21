0efa34d4b8a56461caf60e5b769416d16a2da3ff
lavanyaadapa05 <lavanyaadapa05@gmail.com>
2025-02-21 11:25:04 +0530

    changeserver

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
+
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
